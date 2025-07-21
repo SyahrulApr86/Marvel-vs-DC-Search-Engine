8056cc788965e9170447dcd4e6ec7c6bbfbb307c
SyahrulApr86 <apriansyah.syahrul@gmail.com>
2025-07-22 00:26:26 +0700

    feat(template): add split filter for template tag processing

--- marvel_dc_app/templatetags/math_extras.py
+++ marvel_dc_app/templatetags/math_extras.py
@@ -17,6 +17,16 @@ def add_float(value, arg):
     """Add a float to the value."""
     return value + arg
 
+@register.filter(is_safe=True)
+def split(value, delimiter=', '):
+    """
+    Returns a list of strings after splitting by delimiter.
+    Usage: {{ value|split:", " }}
+    """
+    if value:
+        return value.split(delimiter)
+    return []
+
 @register.filter
 def sub(value, arg):
     """Subtract the argument from the value."""
